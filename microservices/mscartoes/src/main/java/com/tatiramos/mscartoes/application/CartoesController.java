e3d77b9e868e96c2d61ef96df9d019ff90978f80
Tati Ramos <tatiramos@ibm.com>
2023-08-23 17:41:57 -0300

    ✨ Implementando o Microsservico de Cartoes - Parte 1

--- microservices/mscartoes/src/main/java/com/tatiramos/mscartoes/application/CartoesController.java
+++ microservices/mscartoes/src/main/java/com/tatiramos/mscartoes/application/CartoesController.java
@@ -1,15 +1,28 @@
 package com.tatiramos.mscartoes.application;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.tatiramos.mscartoes.application.dto.CartaoSaveRequest.CartaoSaveRequest;
+import com.tatiramos.mscartoes.domain.Cartao;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/cartoes")
+@RequiredArgsConstructor
 public class CartoesController {
+
+    private final CartaoService service;
 
     @GetMapping
     public String status(){
         return "ok";
+    }
+
+    @PostMapping
+    public ResponseEntity cadastra(@RequestBody CartaoSaveRequest request){
+        Cartao cartao = request.toModel();
+        service.save(cartao);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 }
