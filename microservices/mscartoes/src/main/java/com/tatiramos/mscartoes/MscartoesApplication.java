89c4b9163277e33793f8c27a65a009538f9cbb61
Tati Ramos <tatiramos@ibm.com>
2023-08-23 17:14:14 -0300

    ✨ Criando e Configurando o Microsservico de Cartoes

---
+++ microservices/mscartoes/src/main/java/com/tatiramos/mscartoes/MscartoesApplication.java
@@ -0,0 +1,15 @@
+package com.tatiramos.mscartoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class MscartoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MscartoesApplication.class, args);
+	}
+
+}
