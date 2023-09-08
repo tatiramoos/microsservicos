87720631ffcc6248027a3e0a5e613a0c451420f3
Tati Ramos <tatiramos@ibm.com>
2023-09-08 11:42:27 -0300

    ✨ Endpoint de Consulta de Situacao do Cliente

--- microservices/msavaliadorcredito/src/main/java/com/tatiramos/msavaliadorcredito/application/AvaliadorCreditoController.java
+++ microservices/msavaliadorcredito/src/main/java/com/tatiramos/msavaliadorcredito/application/AvaliadorCreditoController.java
@@ -1,16 +1,26 @@
 package com.tatiramos.msavaliadorcredito.application;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.tatiramos.msavaliadorcredito.domain.model.SituacaoCliente;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("avaliacoes-credito")
+@RequiredArgsConstructor
 public class AvaliadorCreditoController {
+
+    private final AvaliadorCreditoService avaliadorCreditoService;
 
     @GetMapping
     public String status(){
         return "ok";
+    }
+
+    @GetMapping(value = "situacao-cliente", params = "cpf")
+    public ResponseEntity<SituacaoCliente> consultaSituacaoCliente(@RequestParam("cpf") String cpf){
+        SituacaoCliente situacaoCliente = avaliadorCreditoService.obterSituacaoCliente(cpf);
+
     }
 }
 
---
+++ microservices/msavaliadorcredito/src/main/java/com/tatiramos/msavaliadorcredito/application/AvaliadorCreditoService.java
@@ -0,0 +1,18 @@
+package com.tatiramos.msavaliadorcredito.application;
+
+import com.tatiramos.msavaliadorcredito.domain.model.SituacaoCliente;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AvaliadorCreditoService {
+
+    public SituacaoCliente obterSituacao (String cpf){
+        // obter dados do cliente - MSCLIENTES
+        // obter cartões do cliente - MSCARTOES
+
+        
+
+    }
+}
