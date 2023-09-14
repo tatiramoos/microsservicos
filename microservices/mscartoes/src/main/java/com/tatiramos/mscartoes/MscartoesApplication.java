362003d35ace4de348d2f30d5d0087346bbc03c4
Tati Ramos <tatiramos@ibm.com>
2023-09-14 09:08:54 -0300

    ✨ Criando o Subscriber para a fila de emissao de cartoes

--- microservices/mscartoes/src/main/java/com/tatiramos/mscartoes/MscartoesApplication.java
+++ microservices/mscartoes/src/main/java/com/tatiramos/mscartoes/MscartoesApplication.java
@@ -1,11 +1,14 @@
 package com.tatiramos.mscartoes;
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+@EnableEurekaClient
+@EnableRabbit
 public class MscartoesApplication {
 
 	public static void main(String[] args) {
