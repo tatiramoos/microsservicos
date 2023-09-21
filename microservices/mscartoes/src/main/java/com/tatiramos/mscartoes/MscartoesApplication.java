acfe2ebde3521788dfb3038b7fef81eec5dfb63c
Tati Ramos <tatiramos@ibm.com>
2023-09-21 10:20:42 -0300

    ✨ Actuator, metricas e arquivo de log

--- microservices/mscartoes/src/main/java/com/tatiramos/mscartoes/MscartoesApplication.java
+++ microservices/mscartoes/src/main/java/com/tatiramos/mscartoes/MscartoesApplication.java
@@ -1,5 +1,6 @@
 package com.tatiramos.mscartoes;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,6 +10,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @SpringBootApplication
 @EnableEurekaClient
 @EnableRabbit
+@Slf4j
 public class MscartoesApplication {
 
 	public static void main(String[] args) {
--- microservices/mscartoes/src/main/java/com/tatiramos/mscartoes/infra/mqueue/EmissaoCartaoSubscriber.java
+++ microservices/mscartoes/src/main/java/com/tatiramos/mscartoes/infra/mqueue/EmissaoCartaoSubscriber.java
@@ -8,12 +8,14 @@ import com.tatiramos.mscartoes.domain.DadosSolicitacaoEmissaoCartao;
 import com.tatiramos.mscartoes.infra.repository.CartaoRepository;
 import com.tatiramos.mscartoes.infra.repository.ClienteCartaoRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class EmissaoCartaoSubscriber {
 
     private final CartaoRepository repository;
@@ -33,7 +35,7 @@ public class EmissaoCartaoSubscriber {
             clienteCartaoRepository.save(clienteCartao);
 
         }catch (Exception e){
-            e.printStackTrace();
+            log.error("Erro ao receber solicitação de emissão de cartão: {}", e.getMessage());
         }
     }
 }
