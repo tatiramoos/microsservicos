b96745b577fb596051f04e36fa11da973272ccda
Tati Ramos <tatiramos@ibm.com>
2023-10-09 18:26:41 -0300

    ✨ Atualizando Projeto

--- microservices/eurekaserver/src/main/java/com/tatiramos/eurekaserver/SecurityConfig.java
+++ microservices/eurekaserver/src/main/java/com/tatiramos/eurekaserver/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.tatiramos.eurekaserver;
 
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableWebSecurity
